e674ee016fd2523c372f6aefc1dbb932a8556bf6
Raymond Wong <90529699+RaymondWKWong@users.noreply.github.com>
2024-07-12 15:40:56 +0000

    Update

--- student/shuffling_cards.py
+++ student/shuffling_cards.py
@@ -3,19 +3,6 @@ import numpy as np
 def find_card_position(deck_size, instructions, card):
     deck = np.arange(deck_size)
 
-    # for instruction in instructions:
-    #     if instruction.startswith("c"):
-    #         n = int(instruction.split()[-1])
-    #         deck = np.concatenate((deck[n:], deck[:n]))
-    #     elif instruction.startswith("d"):
-    #         deck = np.flip(deck)
-    #     elif instruction.startswith("s"):
-    #         half = len(deck) // 2
-    #         new_deck = np.empty_like(deck)
-    #         new_deck[0::2] = deck[:half]
-    #         new_deck[1::2] = deck[half:]
-    #         deck = new_deck
-
     for instruction in instructions:
         if instruction[0] == "c":  # Cut operation
             n = int(instruction.split()[-1])
